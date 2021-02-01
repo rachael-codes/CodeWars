4d57e66c5c822c631bc8fe24626caa89622a9f63
rachael-codes <mcbride.rac@gmail.com>
2021-01-31 20:27:39 -0500

    Update one Code Wars problem to include algorithm

--- largest_product.rb
+++ largest_product.rb
@@ -7,20 +7,17 @@
 # Complete the greatest_product method so that it'll find the greatest product of five 
 # consecutive digits in the given string of digits.
 
-# TEST CASES
-p greatest_product("123834539327238239583") == 3240 # the largest product of five consecutive digits equals the integer 3240
-p greatest_product("395831238345393272382") == 3240 
-p greatest_product("92494737828244222221111111532909999")# == 5292
-p greatest_product("92494737828244222221111111532909999")# == 5292
-p greatest_product("02494037820244202221011110532909999")# == 0
+# Algorithm
+# -create an array of all consecutive 5-digit slice subarrays
+# -convert all subarray eleements into integers
+# -find the subarray that--when all of its integers are multiplied--returns the highest number
+# -return this number
 
 def greatest_product(string)
   array_of_5_digits = string.chars.each_cons(5).to_a
   
   converted = array_of_5_digits.map do |subarray|
-    subarray.map do |element| 
-      element.to_i
-    end
+    subarray.map { |el| el.to_i } 
   end
 
   result = converted.max_by do |subarray| 
@@ -29,3 +26,11 @@ def greatest_product(string)
   
   result.inject(:*)
 end
+
+
+# TEST CASES
+p greatest_product("123834539327238239583") == 3240 # the largest product of five consecutive digits equals the integer 3240
+p greatest_product("395831238345393272382") == 3240 
+p greatest_product("92494737828244222221111111532909999") == 5292
+p greatest_product("92494737828244222221111111532909999") == 5292
+p greatest_product("02494037820244202221011110532909999") == 0
